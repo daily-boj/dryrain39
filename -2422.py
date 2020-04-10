5b6ec4d9195fe05a96329d99753dd0f7b2e29671
dryrain39 <alex689345@gmail.com>
2020-04-10 17:58:25 +0900

    20041017

---
+++ -2422.py
@@ -0,0 +1,35 @@
+F = lambda n: 1 if n <= 1 else n * F(n - 1)
+
+if __name__ == '__main__':
+    input_val = input().split(' ')
+    no_ice_species, no_ex = int(input_val[0]), int(input_val[1])
+
+    duplicate_list = []
+    duplicate_cnt = 0
+
+    for _ in range(no_ex):
+        except_input = input().split(' ')
+        a, b = int(except_input[0]), int(except_input[1])
+
+        for i in range(1, no_ice_species + 1):
+            if a == i or b == i or a > no_ice_species or b > no_ice_species:
+                continue
+
+            x, y, z = a, b, i
+
+            if x > y:
+                x, y = y, x
+            if y > z:
+                y, z = z, y
+            if x > y:
+                x, y = y, x
+
+            dup_element = (x * 1000000) + (y * 1000) + z
+
+            if dup_element in duplicate_list:
+                continue
+            else:
+                duplicate_list.append(dup_element)
+                duplicate_cnt += 1
+
+    print(int((F(no_ice_species) / (F(no_ice_species - 3) * F(3))) - duplicate_cnt))
